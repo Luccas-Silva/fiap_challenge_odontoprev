55b662242219375bc9e4c7ca345c631e8a0c0829
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-09-27 19:55:33 -0300

    starting java project

---
+++ Java_Challenge/src/test/java/com/odontoprev/Java_Challenge/JavaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odontoprev.Java_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
