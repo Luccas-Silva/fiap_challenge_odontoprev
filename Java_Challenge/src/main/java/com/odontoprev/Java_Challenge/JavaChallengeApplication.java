ba74cbc8c674812e8f1967dead0890c536c342ca
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-09-29 18:59:32 -0300

    teste domain

--- Java_Challenge/src/main/java/com/odontoprev/Java_Challenge/JavaChallengeApplication.java
+++ Java_Challenge/src/main/java/com/odontoprev/Java_Challenge/JavaChallengeApplication.java
@@ -26,6 +26,62 @@ public class JavaChallengeApplication {
 		SpringApplication.run(JavaChallengeApplication.class, args);
 	}
 
+	private final ClienteRepository clienteRepository;
+	private final DentistaRepository dentistaRepository;
+	private final ConsultaRepository consultaRepository;
 
+	@EventListener(value = ApplicationReadyEvent.class)
+	public void setupCliente() {
+		Date date = new Date();
+		for (int i=0; i<=4; i++){
+			Cliente clienteCadastrado = Cliente.builder()
+					.usuario(Usuario.builder()
+							.nome("Cliente "+ i)
+							.dataNascimento(date)
+							.Email("cliente"+i+"@gmail.com")
+							.Celular("(11)99999-9999")
+							.build())
+					.cpf_cnpj("1234567891"+i)
+					.cep("0000000"+i)
+					.tipoPlano("Bem Estar White")
+					.build();
+			clienteRepository.save(clienteCadastrado);
+		}
+	}
+
+	@EventListener(value = ApplicationReadyEvent.class)
+	public void setupDentista() {
+		Date date = new Date();
+		for (int i=5; i<=9; i++){
+			Dentista dentistaCadastrado = Dentista.builder()
+					.usuario(Usuario.builder()
+							.nome("Dentista "+ i)
+							.dataNascimento(date)
+							.Email("Dentista"+i+"@gmail.com")
+							.Celular("(11)99999-9999")
+							.build())
+					.cpf_cnpj("1234567891"+i)
+					.cepClinica("0000000"+i)
+					.nomeClinica("Clinica "+i)
+					.tipoClinica("Tipo 1")
+					.alvaraFuncionamento(true)
+					.siteRedesSocial("@Dentista_Odonto")
+					.build();
+			dentistaRepository.save(dentistaCadastrado);
+		}
+	}
+
+	@EventListener(value = ApplicationReadyEvent.class)
+	public void setupConsulta(){
+		for (int i=1; i<=5; i++){
+			Date date = new Date();
+			Consulta consultaCadastrado = Consulta.builder()
+					.dateConsulta(date)
+					.tipoConsulta("Consulta periódica")
+					.valorMedioConsulta(200.00)
+					.build();
+			consultaRepository.save(consultaCadastrado);
+		}
+	}
 
 }
